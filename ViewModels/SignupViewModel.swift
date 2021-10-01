ec49a4dcfb9a48714697b15d81a3cda861c48842
Vineet Singh <vsingh@vexcelco.com>
2021-10-01 14:53:52 -0400

    Create SignupViewModel

---
+++ ViewModels/SignupViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  SignupViewModel.swift
+//  SignupCodeChallenge
+//
+//  Created by Vineet Singh on 10/1/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class SignupViewModel: ObservableObject
+{
+    @Published var firstName: String = ""
+    @Published var emailAddress: String = ""
+    @Published var password: String = ""
+    @Published var website: String = ""
+    @Published var isShowPhotoLibrary = false
+    @Published var enableSubmitButton = false
+
+    let desctiptionText = "Use the form below to submit your portfolio. An email and password are required."
+
+    var isEmailValid : Bool   = false
+    var isWebsiteUrlValid : Bool   = true
+
+    init()
+    {
+    }
+   
+}
+
